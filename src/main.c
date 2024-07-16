8d497405fb93e7a59d07e41cf4c0ef4dec035e0b
Yan Avlis <codeyan10@gmail.com>
2024-07-16 08:47:58 -0300

    main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,48 @@
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define REGISTERS_NUMBER 8
+#define MEM_SIZE 1024
+// define registers;
+typedef enum {
+    EAX = 0,
+    EBX,
+    ECX,
+    EDX,
+    ESI,
+    EDI,
+    ESP,
+    EBP,
+    PC,
+    IC,
+    AC,
+    R1,
+    R2,
+    R3,
+    R4
+} Registers;
+
+typedef struct {
+    size_t *registers;  // Registers pointer
+    size_t *memory;     // Memory pointer
+    size_t memory_size; // Memory size
+    long clock_cycles;  // Counts cycles
+} CPU;
+
+CPU *init(size_t *memory) {
+    CPU *cpu = (CPU *)malloc(sizeof(CPU));
+    cpu->registers = (size_t *)malloc(REGISTERS_NUMBER * sizeof(size_t));
+    cpu->memory = memory;
+    cpu->memory_size = sizeof(memory);
+    cpu->clock_cycles = 0;
+}
 
 int main(int argc, char **argv) {
-    char *message = "Hello, World!";
-
-    printf("%s\n", message);
-    return 0;
+    // size_t memory[MEM_SIZE];
+    //
+    // CPU *cpu = init(&memory);
+    //
+    // printf("%lu", cpu->clock_cycles);
+    printf("hello, world!");
 }
