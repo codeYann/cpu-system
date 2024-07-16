211bb7c36c698c4d910f0e83c346adac9b6491df
Yan Avlis <codeyan10@gmail.com>
2024-07-16 08:53:18 -0300

    Adding fetch, decode and execute functions

--- lib/cpu/cpu.h
+++ lib/cpu/cpu.h
@@ -4,12 +4,18 @@
 #define CPU_H
 
 typedef struct {
-    int PC, IR, AC;     // Define the fetch-decode-execute main registers
-    int R1, R2, R3, R4; // define geral purpose registers
-    int *memory;        // define memory pointer
-    size_t memroy_size;
+    int PC, IR, AC;     // Fetch-decode-execute main registers
+    int R1, R2, R3, R4; // A geral purpose registers
+    int *memory;        // Memory pointer
+    size_t memroy_size; // Memory size
+    long clock_cycles;  // Counts cycles
+    void (*instructions[256])(
+        struct CPU *cpu); // Array of function pointers for instruction
 } CPU;
 
 void init(CPU *cpu, int *memory);
+void fetch(CPU *cpu);
+void decode(CPU *cpu);
+void execute(CPU *cpu);
 
 #endif // CPU_H
